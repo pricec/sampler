b7091164c79ba201d3ef144a8eaa4dd7bec4ee26
Chris Price <chrisprice@rogers.com>
2018-05-29 18:27:17 -0400

    Add config handling to skeleton, include more dependencies

--- main.go
+++ main.go
@@ -1,7 +1,8 @@
 package main
 
-import(
+import (
 	"fmt"
+	"io/ioutil"
 	"os"
 	"os/signal"
 	"syscall"
@@ -9,30 +10,81 @@ import(
 	"golang.org/x/net/context"
 
 	"github.com/go-yaml/yaml"
+	"github.com/jawher/mow.cli"
 )
 
+type Config struct {
+	Path    string                      // Path to configuration file
+	Verbose bool                        // Verbose logging mode?
+	Items   []ConfigItem `yaml:"items"` // Items to sample
+}
+
+type ConfigItem struct {
+	Kind     string `yaml:"type"`     // Type of sample (file, command, etc)
+	Interval string `yaml:"interval"` // Sampling interval
+	Path     string `yaml:"path"`     // Path to file or command to run, etc
+	Metric   string `yaml:"metric"`   // Type of metric
+}
+
+func populateConfig(cfg *Config) error {
+	data, err := ioutil.ReadFile(cfg.Path)
+	if err == nil {
+		err = yaml.Unmarshal(data, cfg)
+	}
+	return err
+}
+
 func sigHandler(
+	ctx context.Context,
 	cancel context.CancelFunc,
 	sigChan <- chan os.Signal,
 	wantExit *bool,
 ) {
-	<- sigChan
-	*wantExit = true
-	cancel()
+	select {
+	case sig := <- sigChan:
+		if sig == syscall.SIGINT || sig == syscall.SIGTERM {
+			*wantExit = true
+		}
+		cancel()
+	case <- ctx.Done():
+	}
 }
 
-func main() {
-	ctx, cancel := context.WithCancel(context.Background())
+func mainLoop(cfg *Config) {
 	wantExit    := false
 	sigChan     := make(chan os.Signal)
 
-	signal.Notify(sigChan, syscall.SIGTERM, syscall.SIGINT)
-	go sigHandler(cancel, sigChan, &wantExit)
+	signal.Notify(sigChan, syscall.SIGTERM, syscall.SIGINT, syscall.SIGHUP)
 
 	for !wantExit {
-		fmt.Println("Waiting for context cancel")
+		ctx, cancel := context.WithCancel(context.Background())
+		go sigHandler(context.Background(), cancel, sigChan, &wantExit)
+		if err := populateConfig(cfg); err != nil {
+			fmt.Printf("Failed to read configuration: %v\n", err)
+			sigChan <- syscall.SIGTERM
+		}
 		<-ctx.Done()
-		fmt.Println("Got context cancel")
 	}
-	fmt.Println("Exiting")
+}
+
+func main() {
+	app := cli.App("sampler", "Sample values and send to statsd")
+
+	app.Spec = "[-v] CONFIG_FILE"
+
+	var (
+		verbose = app.BoolOpt("v verbose", false, "Verbose logging mode")
+		cfgFile = app.StringArg("CONFIG_FILE", "", "Path to config file")
+	)
+
+	app.Action = func() {
+		cfg := Config{
+			Path:    *cfgFile,
+			Verbose: *verbose,
+		}
+
+		mainLoop(&cfg)
+	}
+
+	app.Run(os.Args)
 }
