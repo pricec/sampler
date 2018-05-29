05d78b333feea2565bf95597b1c491115bfd8cee
Chris Price <chrisprice@rogers.com>
2018-05-29 17:21:42 -0400

    Basic skeleton

---
+++ main.go
@@ -0,0 +1,36 @@
+package main
+
+import(
+	"fmt"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"golang.org/x/net/context"
+)
+
+func sigHandler(
+	cancel context.CancelFunc,
+	sigChan <- chan os.Signal,
+	wantExit *bool,
+) {
+	<- sigChan
+	*wantExit = true
+	cancel()
+}
+
+func main() {
+	ctx, cancel := context.WithCancel(context.Background())
+	wantExit    := false
+	sigChan     := make(chan os.Signal)
+
+	signal.Notify(sigChan, syscall.SIGTERM, syscall.SIGINT)
+	go sigHandler(cancel, sigChan, &wantExit)
+
+	for !wantExit {
+		fmt.Println("Waiting for context cancel")
+		<-ctx.Done()
+		fmt.Println("Got context cancel")
+	}
+	fmt.Println("Exiting")
+}
